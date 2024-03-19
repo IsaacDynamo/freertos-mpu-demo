b245a971a0598ced66a65e2c12e017ac29272be7
IsaacDynamo <isaacdynamo@gmail.com>
2024-03-19 22:01:21 +0100

    clean-up build.rs

--- stm32l452-nucleo-demo/build.rs
+++ stm32l452-nucleo-demo/build.rs
@@ -1,14 +1,10 @@
 fn main() {
-    //println!("cargo:rustc-link-search=native=../../freertos-main");
-    //println!("cargo:rustc-link-lib=c_main");
     println!("cargo:rustc-link-search=native=../build/FreeRTOS-Kernel");
     println!("cargo:rustc-link-lib=freertos_kernel");
     println!("cargo:rustc-link-search=native=../build/FreeRTOS-Kernel/portable");
     println!("cargo:rustc-link-lib=freertos_kernel_port");
 
     println!("cargo:rerun-if-changed=build.rs");
-    //println!("cargo:rerun-if-changed=../../freertos-main/libc_main.a");
     println!("cargo:rerun-if-changed=../build/FreeRTOS-Kernel/libfreertos_kernel.a");
     println!("cargo:rerun-if-changed=../build/FreeRTOS-Kernel/portable/libfreertos_kernel_port.a");
 }
-
